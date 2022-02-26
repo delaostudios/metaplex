33af11ae7a4ff91971b04050d10f26356897c688
delaostudios <delaostudios@gmail.com>
2022-02-26 12:49:53 -0500

    Updates

---
+++ _next/static/eEWXZy0ZaqkHnkoxVNhQM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-f3123a521bf52d553ad2.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
